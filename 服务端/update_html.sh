8ee02832599bd61ffb939bf0848fbc9108fb015b
yangsir <94435405+sensir233@users.noreply.github.com>
2023-01-24 10:34:53 +0800

    Update update_html.sh

--- 服务端/update_html.sh
+++ 服务端/update_html.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+#我的文件结构
+#/--www--wwwroot--IP地址--picture---------图片
+#                         |                |
+#                         |                filename.txt
+#                         |
+#                         index.html
+#                         |
+#                          update_html.sh
+
+path="绝对路径"
 src_x="2023-01-23_1500"
 src_y="2023-01-23-1600"
-src_x=$(grep './picture/' index.html | awk -F/ '{print $3}' | awk -F. '{print $1}'|xargs)
-src_y=$(sed -nr '/'"$src_x"'/{n;p}' ./picture/filename.txt|awk -F. '{print $1}' |xargs )
-sed -i 's/'"$src_x"'/'"$src_y"'/g' index.html
+src_x=$(grep './picture/' $path/index.html | awk -F/ '{print $3}' | awk -F. '{print $1}'|xargs)
+src_y=$(sed -nr '/'"$src_x"'/{n;p}' $path/picture/filename.txt|awk -F. '{print $1}' |xargs )
+sed -i 's/'"$src_x"'/'"$src_y"'/g' $path/index.html
